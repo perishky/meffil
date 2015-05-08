7afbbee561c3abb7bde06245c9357304c0210a00
Matthew Suderman <matthewsuderman@hotmail.com>
2015-05-08 11:08:30 +0100

    adds Slide to create sample sheet, fixes Sample_Name bug

--- R/create-samplesheet.r
+++ R/create-samplesheet.r
@@ -7,32 +7,41 @@
 #' @param  delim Optional delim character to separate \code{Sample_Name} into multiple columns. Default: "_"
 #' @export
 #' @return Sample sheet data frame
-meffil.create.samplesheet <- function(path, Sample_Name = NULL, Sex = NULL, delim = "_")
-{
-	basenames <- meffil.basenames(path)
-	if(is.null(Sex)) Sex <- rep(NA, length(basenames))
-	if(any(!Sex %in% c("M", "F", NA))) stop("Sex column must only contain 'M', 'F' or NA values")
-	if(is.null(Sample_Name)) Sample_Name <- make.samplename.from.basename(basenames)
-	stopifnot(length(Sample_Name) == length(basenames))
-	stopifnot(length(Sex) == length(basenames))
-
-	dat <- data.frame(do.call(rbind, strsplit(basename(basenames), split=delim)), stringsAsFactors=FALSE)
-	idcol <- which(apply(dat, 2, function(x) all(!duplicated(x))))
-	if(length(idcol) == 1)
-	{
-		Sample_Name <- dat[,idcol]
-		dat <- dat[,-idcol]
-	}
-	sentrixpos <- grep("^R[0-9][0-9]C[0-9][0-9]$", as.character(unlist(dat[1,])))
-	if(length(sentrixpos)==1)
-	{
-		temp <- do.call(rbind, strsplit(as.character(dat[,sentrixpos]), split="C"))
-		dat$sentrix_row <- gsub("R", "", temp[,1])
-		dat$sentrix_col <- temp[,2]
-		dat <- dat[,-sentrixpos]
-	}
-	samplesheet <- data.frame(Sample_Name = Sample_Name, Sex = Sex, dat, Basename = basenames, stringsAsFactors=FALSE)
-	return(samplesheet)
+meffil.create.samplesheet <- function(path, Sample_Name = NULL, Sex = NULL, delim = "_") {
+    basenames <- meffil.basenames(path)
+    
+    if(is.null(Sex)) Sex <- rep(NA, length(basenames))
+    if(any(!Sex %in% c("M", "F", NA))) stop("Sex column must only contain 'M', 'F' or NA values")
+    stopifnot(length(Sex) == length(basenames))
+    
+    dat <- data.frame(do.call(rbind, strsplit(basename(basenames), split=delim)), stringsAsFactors=FALSE)
+    idcol <- which(apply(dat, 2, function(x) all(!duplicated(x))))
+    if (is.null(Sample_Name)) {
+        if(length(idcol) >= 1) {
+            Sample_Name <- dat[,idcol[1]]
+            dat <- dat[,-idcol[1]]
+        }
+        else {
+            Sample_Name <- make.samplename.from.basename(basenames)
+        }
+    }
+    stopifnot(length(Sample_Name) == length(basenames))
+    
+    sentrixpos <- grep("^R[0-9][0-9]C[0-9][0-9]$", as.character(unlist(dat[1,])))
+    if(length(sentrixpos)==1) {
+        temp <- do.call(rbind, strsplit(as.character(dat[,sentrixpos]), split="C"))
+        dat$sentrix_row <- gsub("R", "", temp[,1])
+        dat$sentrix_col <- temp[,2]
+        dat <- dat[,-sentrixpos]
+    }
+    
+    slidecol <- grep("^[0-9]{9}[0-9]*$", as.character(unlist(dat[1,])))
+    if (length(slidecol) == 1) {
+        colnames(dat)[slidecol] <- "Slide"
+    }
+    
+    samplesheet <- data.frame(Sample_Name = Sample_Name, Sex = Sex, dat, Basename = basenames, stringsAsFactors=FALSE)
+    return(samplesheet)
 }
 
 
