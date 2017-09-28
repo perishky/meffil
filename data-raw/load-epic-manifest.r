f66ad7550d4435979a543efe621bfc94706f9bf8
Matthew Suderman <matthewsuderman@hotmail.com>
2017-09-28 13:42:20 +0100

    new EPIC manifest v4

--- data-raw/load-epic-manifest.r
+++ data-raw/load-epic-manifest.r
@@ -1,7 +1,8 @@
 load.epic.manifest <- function() {
     ## filename <- "ftp://webdata2:webdata2@ussd-ftp.illumina.com/downloads/productfiles/methylationEPIC/infinium-methylationepic-v1-0-b1-manifest-file-csv.zip"
     ## filename <- "ftp://webdata2:webdata2@ussd-ftp.illumina.com/downloads/productfiles/methylationEPIC/infinium-methylationepic-v1-0-b2-manifest-file-csv.zip"
-    filename <- "ftp://webdata2:webdata2@ussd-ftp.illumina.com/downloads/productfiles/methylationEPIC/infinium-methylationepic-v1-0-b3-manifest-file-csv.zip"
+    ## filename <- "ftp://webdata2:webdata2@ussd-ftp.illumina.com/downloads/productfiles/methylationEPIC/infinium-methylationepic-v1-0-b3-manifest-file-csv.zip"
+    filename <- "ftp://webdata2:webdata2@ussd-ftp.illumina.com/downloads/productfiles/methylationEPIC/infinium-methylationepic-v-1-0-b4-manifest-file-csv.zip"
 
     on.exit(unlink(basename(filename)))
     cat("Downloading", filename, "\n")
@@ -21,14 +22,15 @@ load.epic.manifest <- function() {
     manifest$AddressB_ID <- sub("^0*", "", manifest$AddressB_ID)
 
     ## some addresses in the manifest don't appear in idat files
-    excluded.addresses <- readLines("epic-excludes.txt")
-    idx <- which(manifest$AddressA_ID %in% excluded.addresses
-                 | manifest$AddressB_ID %in% excluded.addresses
-                 | manifest$IlmnID %in% excluded.addresses
-                 | manifest$IlmnID %in% excluded.addresses
-                 | as.character(manifest$IlmnID) == "[Controls]")    
+    ## excluded.addresses <- readLines("epic-excludes.txt")
+    ## idx <- which(manifest$AddressA_ID %in% excluded.addresses
+    ##              | manifest$AddressB_ID %in% excluded.addresses
+    ##              | manifest$IlmnID %in% excluded.addresses
+    ##              | manifest$IlmnID %in% excluded.addresses
+    ##              | as.character(manifest$IlmnID) == "[Controls]")
+    idx <- which(as.character(manifest$IlmnID) == "[Controls]")
     if (length(idx) > 0)
-        manifest <- manifest[-idx,]
+         manifest <- manifest[-idx,]
     
     manifest
 }
