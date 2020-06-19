1db95c401bcce6a92d01a35961c2a7036a6f574c
Matthew Suderman <matthewsuderman@hotmail.com>
2020-06-19 12:04:49 +0100

    updates epic annotation to mar2020

--- data-raw/load-epic-manifest.r
+++ data-raw/load-epic-manifest.r
@@ -2,7 +2,8 @@ load.epic.manifest <- function() {
     ## filename <- "ftp://webdata2:webdata2@ussd-ftp.illumina.com/downloads/productfiles/methylationEPIC/infinium-methylationepic-v1-0-b1-manifest-file-csv.zip"
     ## filename <- "ftp://webdata2:webdata2@ussd-ftp.illumina.com/downloads/productfiles/methylationEPIC/infinium-methylationepic-v1-0-b2-manifest-file-csv.zip"
     ## filename <- "ftp://webdata2:webdata2@ussd-ftp.illumina.com/downloads/productfiles/methylationEPIC/infinium-methylationepic-v1-0-b3-manifest-file-csv.zip"
-    filename <- "ftp://webdata2:webdata2@ussd-ftp.illumina.com/downloads/productfiles/methylationEPIC/infinium-methylationepic-v-1-0-b4-manifest-file-csv.zip"
+    ## filename <- "ftp://webdata2:webdata2@ussd-ftp.illumina.com/downloads/productfiles/methylationEPIC/infinium-methylationepic-v-1-0-b4-manifest-file-csv.zip"
+    filename <- "ftp://webdata2:webdata2@ussd-ftp.illumina.com/downloads/productfiles/methylationEPIC/infinium-methylationepic-v-1-0-b5-manifest-file-csv.zip"
 
     on.exit(unlink(basename(filename)))
     cat("Downloading", filename, "\n")
@@ -15,7 +16,11 @@ load.epic.manifest <- function() {
                                      skip=7, stringsAsFactors=F)) ## 5 minutes
     
     ## add snp exclusions
-    manifest$snp.exclude <- manifest$Name %in% with(manifest, Name[which(as.numeric(as.character(SNP_MinorAlleleFrequency)) > 0.01)])
+    freq <- manifest$SNP_MinorAlleleFrequency
+    freq <- strsplit(freq, ";")
+    freq <- lapply(freq, as.numeric)
+    freq <- sapply(freq, max)
+    manifest$snp.exclude <- manifest$Name %in% manifest$Name[which(freq > 0.01)]
 
     ## remove leading 0's from probes addresses
     manifest$AddressA_ID <- sub("^0*", "", manifest$AddressA_ID)
@@ -23,12 +28,11 @@ load.epic.manifest <- function() {
 
     ## some addresses in the manifest don't appear in idat files
     ## excluded.addresses <- readLines("epic-excludes.txt")
-    ## idx <- which(manifest$AddressA_ID %in% excluded.addresses
-    ##              | manifest$AddressB_ID %in% excluded.addresses
-    ##              | manifest$IlmnID %in% excluded.addresses
-    ##              | manifest$IlmnID %in% excluded.addresses
-    ##              | as.character(manifest$IlmnID) == "[Controls]")
-    idx <- which(as.character(manifest$IlmnID) == "[Controls]")
+    idx <- which(manifest$AddressA_ID %in% excluded.addresses
+                 | manifest$AddressB_ID %in% excluded.addresses
+                 | manifest$IlmnID %in% excluded.addresses
+                 | manifest$IlmnID %in% excluded.addresses
+                 | as.character(manifest$IlmnID) == "[Controls]")
     if (length(idx) > 0)
          manifest <- manifest[-idx,]
     
