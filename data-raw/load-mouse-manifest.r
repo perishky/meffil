17b6256eedc5b65770f4bd3b317938a5609774da
Matthew Suderman <matthewsuderman@hotmail.com>
2021-02-08 11:28:07 +0000

    Create load-mouse-manifest.r

---
+++ data-raw/load-mouse-manifest.r
@@ -0,0 +1,3 @@
+## https://support.illumina.com/array/array_kits/infinium-mouse-methylation-beadchip-kit/downloads.html
+## https://support.illumina.com/content/dam/illumina-support/documents/downloads/productfiles/mouse-methylation/Infinium%20Mouse%20Methylation%20v1.0%20A1%20GS%20Manifest%20File.csv
+## https://support.illumina.com/content/dam/illumina-support/documents/downloads/productfiles/mouse-methylation/MouseMethylation-12v1-0_A1_Annotation_Mus_musculus.csv
