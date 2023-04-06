6c0278f9c6e729834d5d8b72eca6b96ddf956a2c
Matthew Suderman <matthewsuderman@hotmail.com>
2023-04-06 20:09:13 +0100

    fixes epic2 replicate probes

--- data-raw/load-epic2-manifest.r
+++ data-raw/load-epic2-manifest.r
@@ -15,6 +15,9 @@ load.epic2.manifest <- function() {
     freq <- manifest$SNP_MinorAlleleFrequency
     freq <- strsplit(freq, ";")
     freq <- lapply(freq, as.numeric)
+    L <- sapply(freq,length)
+    if (any(L==0))
+        freq[which(L==0)] <- 0
     freq <- sapply(freq, max)
     manifest$snp.exclude <- manifest$Name %in% manifest$Name[which(freq > 0.01)]
     ## appears to be SNPs in probe with MAF at least 0.01
@@ -32,6 +35,20 @@ load.epic2.manifest <- function() {
                  | as.character(manifest$IlmnID) == "[Controls]")
     if (length(idx) > 0)
          manifest <- manifest[-idx,]
+
+    ## a few thousand cytosines are measured multiple times on the microarray
+    ## using the same probe sequences each time
+    ## we'll make all available but append the duplicated probes with the Illumina ID
+    ## which includes a duplication number
+    probe.names <- manifest$Name[grep("^(cg|ch|rs)", manifest$Name)]
+    probe.freq <- table(probe.names)
+    duplicate.probes <- names(which(probe.freq > 1))
+    dup.idx <- which(manifest$Name %in% duplicate.probes)
+    dup.idx <- setdiff(dup.idx, match(duplicate.probes, manifest$Name)) ## omit the first time a probe appears
+    manifest$Name[dup.idx] <- manifest$IlmnID[dup.idx]
+    ## check
+    stopifnot(all(duplicate.probes %in% manifest$Name)) ## all duplicated probes appear >= 1 time
+    stopifnot(all(table(manifest$Name[grep("^(cg|ch|rs)", manifest$Name)]))==1) ## all probe names appear 1 time
     
     manifest
 }
